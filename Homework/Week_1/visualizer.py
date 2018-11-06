3db9dd4cd33c6a94028f7b8326369f63778b7497
Zhongyu Chen <zhongyuchen@yahoo.com>
2018-11-06 09:03:29 +0100

    start visualizer.py

---
+++ Homework/Week_1/visualizer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Name:
+# Student number:
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+import csv
+import matplotlib.pyplot as plt
+
+# Global constants for the input file, first and last year
+INPUT_CSV = "movies.csv"
+START_YEAR = 2008
+END_YEAR = 2018
+
+# Global dictionary for the data
+data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
+
+if __name__ == "__main__":
+    print(data_dict)
