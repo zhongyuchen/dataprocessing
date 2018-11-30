03cf70e2fde6a73a42adf5788521a299c63724fc
Zhongyu Chen <zhongyuchen@yahoo.com>
2018-11-30 16:29:59 +0100

    correction

--- Homework/Week_4/bar.js
+++ Homework/Week_4/bar.js
@@ -1,3 +1,63 @@
+window.onload = function() {
+    readcsv('data.csv');
+};
+
+function readcsv(filename) {
+    d3.csv(filename).then(function(csvdata) {
+        // read csv file
+
+        // build all the texts on html
+        text();
+
+        // x and y array
+        var dataset = [];
+        var country = [];
+
+        // store x and y
+        for (let i = 0; i < csvdata.length; i++) {
+            if (csvdata[i]["LOCATION"] !== "OECD" &&
+                csvdata[i]["FREQUENCY"] === "A" &&
+                csvdata[i]["Flag Codes"] === "" &&
+                csvdata[i]["INDICATOR"] === "RENEWABLE" &&
+                csvdata[i]["MEASURE"] === "KTOE" &&
+                csvdata[i]["SUBJECT"] === "TOT" &&
+                csvdata[i]["TIME"] === "2016" &&
+                csvdata[i]["Value"] !== "") {
+                dataset.push(parseFloat(csvdata[i]["Value"]));
+                country.push(csvdata[i]["LOCATION"]);
+            }
+        }
+        // console.log(dataset);
+        // console.log(country);
+
+        // draw the bar chart
+        barchart(dataset, country);
+    });
+}
+
+function text() {
+    // build all the texts on html
+
+    // title
+    d3.select("head").append("title").text("Week_4 Barchart");
+
+    // data sourse
+    d3.select("#source").append("a").text("Data Source").attr("href", "https://data.oecd.org/energy/renewable-energy.htm");
+
+    // name and student number
+    d3.select("#nameandnumber").append("a").attr("class", "white-text").text("Zhongyu Chen, 12455822");
+    // email
+    d3.select("#email").append("a").attr("class", "white-text").text("zhongyuchen@yahoo.com");
+
+    // description
+    d3.select("#description").append("p")
+        .text("Renewable energy measured in KTOE (thousand tonne of oil equivalent) of 35 countries in 2016. " +
+            "OECD (in total) has 512203.649 KTOE. " +
+            "USA has the highest 152533.691 KTOE. " +
+            "LUX has the lowest 206.526 KTOE. " +
+            "Hover over any bar for its exact value!");
+}
+
 function barchart(dataset, country) {
     // draw bar chart
 
