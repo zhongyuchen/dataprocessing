f65da665b39a720d049c3d5f717e94f796157b85
Zhongyu Chen <zhongyuchen@yahoo.com>
2018-12-16 00:56:12 +0100

    dataset chosen

--- Homework/Week_6/static/js/linkedviews.js
+++ Homework/Week_6/static/js/linkedviews.js
@@ -1,3 +1,15 @@
 // Zhongyu Chen, 12455822, zhongyuchen@yahoo.com
 // linkedviews.js for drawing the linked views
 
+window.onload = function() {
+    readcsv('data/asylum_seekers.csv');
+};
+
+function readcsv(filename) {
+    d3.csv(filename).then(function(csvdata) {
+        // read csv file
+
+        console.log(csvdata);
+
+    });
+}
